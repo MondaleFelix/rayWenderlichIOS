c77f987ec4b29ea4b12ac6a35be85b25a215831a
MondaleFelix <mondalefelix@yahoo.com>
2019-03-13 04:00:55 -0700

    added aboutViewController

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Mondale on 3/13/19.
+//  Copyright © 2019 Mondale. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+}

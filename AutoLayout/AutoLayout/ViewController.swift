6aebe0d5af07577f13685657b14a3b835ae0af31
MondaleFelix <mondalefelix@yahoo.com>
2019-09-04 13:35:14 -0700

    added scaleAspectFit property to imageView

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -10,56 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // {} is referred to as closure, or anon. functions
-    let supremeImageView: UIImageView = {
-        let image = UIImage(named: "supreme")
-        let imageView = UIImageView(image: image)
-        // This enables autolayout for our imageView
-        imageView .translatesAutoresizingMaskIntoConstraints = false
+    let bearImageView : UIImageView = {
+        let image = UIImage( named : "bear" )
+        let imageView = UIImageView(image : image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        // Makes sure the images width and height stay the same
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
-    let descriptionTextView: UITextView = {
+    let descriptionTextView : UITextView = {
         let textView = UITextView()
-        textView.text = "Join us today for our fun and games!"
+        textView.text = "Join us today for some fun and games!"
         textView.font = UIFont.boldSystemFont(ofSize: 18)
         textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.textAlignment = .center
         textView.isEditable = false
         textView.isScrollEnabled = false
+        textView.textAlignment = .center
         return textView
     }()
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.addSubview(supremeImageView)
+        
+        view.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
         
         setupLayout()
+
+
+
     }
     
-    func setupLayout(){
-        // Constraints
+    private func setupLayout(){
+        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive =  true
+        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
         
-        // Constraints that centerin the X and Y axis
-        supremeImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
-        //Constraint that sets the image 100 pixels from the top
-        // Top starts from the notch if on iPhone X
-        supremeImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        
-        // Constraints that specify the width and the height
-        supremeImageView.widthAnchor.constraint(equalToConstant: 300).isActive = true
-        supremeImageView.heightAnchor.constraint(equalToConstant: 300).isActive = true
-        
-        
-        descriptionTextView.topAnchor.constraint(equalTo: supremeImageView.bottomAnchor, constant: 150).isActive = true
+        descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 120).isActive = true
         descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
 
     }
 

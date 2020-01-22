f343b8db2945afb5da96dc7a73862e330210517d
MondaleFelix <mondalefelix@yahoo.com>
2020-01-21 19:05:31 -0800

    linked label to data source

--- Checklists2/Checklists/Checklists/ChecklistViewController.swift
+++ Checklists2/Checklists/Checklists/ChecklistViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ChecklistViewController : UITableViewController {
+class ChecklistViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    // Determines the number of cells to display
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
+    // Returns a copy of the prototype cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        // Force casting the view with tag: 1000 as a UILabel
+        let label = cell.viewWithTag(1000) as! UILabel
+        if indexPath.row == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row == 2 {
+            label.text = "Learn iOS Development"
+        } else if indexPath.row == 3 {
+            label.text = "Soccer Practice"
+        } else if indexPath.row == 4 {
+            label.text = "Eat icecream"
+        }
         return cell
+        
     }
 }
